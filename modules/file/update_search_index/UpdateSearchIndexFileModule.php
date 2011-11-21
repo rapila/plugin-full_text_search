523bb99654a55874f3a05141c134175ebae8bde0
Raphael Schweikert <any@sabberworm.com>
2011-11-21 14:14:34 +0100

    intermediate state

--- modules/file/update_search_index/UpdateSearchIndexFileModule.php
+++ modules/file/update_search_index/UpdateSearchIndexFileModule.php
@@ -1,10 +1,42 @@
 <?php
 class UpdateSearchIndexFileModule extends FileModule {
+	private $aIndexPaths;
+	private $sLanguageId;
+	private $oRootNavigationItem;
+	
 	public function __construct($aRequestPath) {
 		parent::__construct($aRequestPath);
+		$this->sLanguageId = Manager::usePath();
+		if($this->sLanguageId === null) {
+			throw new Exception('No language given');
+		}
+		if(!isset($_REQUEST['secret']) || $_REQUEST['secret'] !== Settings::getSetting('full_text_search', 'update_secret', null)) {
+			throw new Exception('Not Authorized');
+		}
+		$this->aIndexPaths = array();
 	}
 	
 	public function renderFile() {
+		//Clear index
+		SearchIndexQuery::create()->filterByLanguageId()->deleteAll();
+		//Spider index
+		$oRootPage = PagePeer::getRootPage();
+		$this->oRootNavigationItem = PageNavigationItem::navigationItemForPage($oRootPage);
+		$this->spider($this->oRootNavigationItem);
 		
+		Util::dumpAll($this->aIndexPaths);
+		
+	}
+	
+	private function spider($oNavigationItem) {
+		FilterModule::getFilters()->handleNavigationPathFound($this->oRootNavigationItem, $oNavigationItem);
+		$this->aIndexPaths[] = $oNavigationItem->getLink();
+		foreach($oNavigationItem->getChildren($this->sLanguageId, true, true) as $oSubNavigationItem) {
+			$this->spider($oSubNavigationItem);
+		}
+	}
+
+	private function indexForPage(Page $oPage) {
+		FrontendManager::$CURRENT_PAGE = $oPage;
 	}
 }
--- modules/filter/search/SearchFilterModule.php
+++ modules/filter/search/SearchFilterModule.php
@@ -1,95 +1,15 @@
 <?php
 class SearchFilterModule extends FilterModule {
-	public function onAnyError($aContainer) {
-		$aError = &$aContainer[0];
-		
-	}
-	
-	public function onBeforePageFill($oCurrentPage, $oTemplate) {
-		
-	}
-	
-	public function onDefaultPageTypeFilledContainer($oContainer, $oPage, $oTemplate, $oFrontendTemplate, $iModuleId) {
-		
-	}
-	
-	public function onDefaultPageTypeFilledContainerWithModule($oContentObject, $oModule, $oTemplate, $oFrontendTemplate, $iModuleId) {
-		
-	}
-	
-	public function onErrorEmailSend($aContainer) {
-		$sAddress = &$aContainer[0];
-		
-	}
-	
-	public function onErrorLog($aContainer) {
-		$sLogFilePath = &$aContainer[0];
-		$aError = &$aContainer[1];
-		$sErrorMessage = &$aContainer[2];
-		$iMode = &$aContainer[3];
-		$sDestination = &$aContainer[4];
-		
-	}
-	
-	public function onErrorPrint($aContainer) {
-		$aError = &$aContainer[0];
-		
-	}
-	
 	public function onFillPageAttributes($oCurrentPage, $oTemplate) {
-		
-	}
-	
-	public function onFillPageAttributesFinished($oCurrentPage, $oTemplate) {
-		
-	}
-	
-	public function onMailGroups($aContainer) {
-		$aMailGroups = &$aContainer[0];
-		
-	}
-	
-	public function onMailGroupsRecipients($aMailGroups, $aContainer) {
-		$aRecipients = &$aContainer[0];
-		
-	}
-	
-	public function onNavigationItemChildrenRequested($oNavigationItem) {
-		
-	}
-	
-	public function onNavigationPathFound($oRootNavigationItem, $oMatchingNavigationItem) {
-		
-	}
-	
-	public function onPageHasBeenSet($oCurrentPage, $bIsNotFound, $oCurrentNavigationItem) {
-		
-	}
-	
-	public function onPageNotFound() {
-		
-	}
-	
-	public function onPageNotFoundDetectionComplete($bIsNotFound, $oCurrentPage, $oCurrentNavigationItem, $aContainer) {
-		$bIsNotFoundMutable = &$aContainer[0];
-		
-	}
-	
-	public function onRequestFinished($aContainer) {
-		$oCurrentPage = &$aContainer[0];
-		$bIsDynamic = &$aContainer[1];
-		$bIsAjaxRequest = &$aContainer[2];
-		$bIsCached = &$aContainer[3];
-		
-	}
-	
-	public function onRichtextWriteTagForIdentifier($sTagName, $aContainer, $oIdentifier, $sTagContent, $mCallbackContext) {
-		$aParameters = &$aContainer[0];
-		
-	}
-	
-	public function onUserLoggedIn($oUser, $aContainer) {
-		$iUserLoginBitmap = &$aContainer[0];
-		
+		$oSearchPage = $oCurrentPage->getPageOfType('search_result');
+		if($oSearchPage === null) {
+			return;
+		}
+		$oTemplate->replaceIdentifier("search_action", LinkUtil::link($oSearchPage->getLinkArray()));
+		foreach($oTemplate->identifiersMatching('search_form', Template::$ANY_VALUE) as $oIdentifier) {
+			$oSubTemplate = new Template($oIdentifier->getValue() ? $oIdentifier->getValue() : 'default', array(DIRNAME_TEMPLATES, 'search_form'));
+			$oSubTemplate->replaceIdentifier("search_action", LinkUtil::link($oSearchPage->getLinkArray()));
+			$oTemplate->replaceIdentifier($oIdentifier, $oSubTemplate);
+		}
 	}
 }
