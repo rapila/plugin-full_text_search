2c0aa1ccb5f6ddb4c15aeef4906779482ded31ad
Jürg Messmer <jm@mosaics.ch>
2019-12-26 14:32:33 +0100

    fix select array notation

--- lib/model/om/BaseSearchIndexWordQuery.php
+++ lib/model/om/BaseSearchIndexWordQuery.php
@@ -853,7 +853,7 @@ abstract class BaseSearchIndexWordQuery extends ModelCriteria
     }
     public function findMostRecentUpdate($bAsTimestamp = false) {
         $oQuery = clone $this;
-        $sDate = $oQuery->clearOrderByColumns()->lastUpdatedFirst()->select("UpdatedAt")->findOne();
+        $sDate = $oQuery->clearOrderByColumns()->lastUpdatedFirst()->select(["UpdatedAt"])->findOne();
         if($sDate === null) {
             return null;
         }
